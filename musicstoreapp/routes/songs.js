eeb00b823dced919efdcde880a5e02926b727498
lauratbg <UO275725@uniovi.es>
2024-03-10 20:21:13 +0100

    SDI-2324-207-8.5-Vistas y motores de plantillas.

--- musicstoreapp/routes/songs.js
+++ musicstoreapp/routes/songs.js
@@ -1,18 +1,21 @@
 
 module.exports = function (app) {
 
-    app.get("/songs", function (req, res) {
-        res.send("lista de canciones");
-    });
-    app.get("/songs", function (req, res) {
-        let response = 'Titulo: ' + req.query.title + '<br>'
-            + 'Autor: ' + req.query.author;
-        res.send(response);
-    });
-    app.get('/add', function(req, res) {
-        let response = parseInt(req.query.num1) + parseInt(req.query.num2);
-        res.send(String(response));
-
+    // app.get("/songs", function (req, res) {
+    //     res.send("lista de canciones");
+    // });
+    // app.get("/songs", function (req, res) {
+    //     let response = 'Titulo: ' + req.query.title + '<br>'
+    //         + 'Autor: ' + req.query.author;
+    //     res.send(response);
+    // });
+    // app.get('/add', function(req, res) {
+    //     let response = parseInt(req.query.num1) + parseInt(req.query.num2);
+    //     res.send(String(response));
+    //
+    // });
+    app.get('/songs/add', function (req, res) {
+        res.render("add.twig");
     });
     app.get('/songs/:id', function(req, res) {
         let response = 'id: ' + req.params.id;
@@ -23,19 +26,36 @@ module.exports = function (app) {
             + 'Tipo de música: ' + req.params.kind;
         res.send(response);
     });
-    app.post('/songs/add', function(req, res){
-       let response = "Canción agregada: " + req.body.title + "<br>"
-       + " género:" + req.body.kind + "<br>"
-        + " precio: " + req.body.price
-
-        res.send(response);
-    });
+    // app.post('/songs/add', function(req, res){
+    //    let response = "Canción agregada: " + req.body.title + "<br>"
+    //    + " género:" + req.body.kind + "<br>"
+    //     + " precio: " + req.body.price
+    //
+    //     res.send(response);
+    // });
     app.get('/promo*', function (req, res) {
-
         res.send('Respuesta al patrón promo*');
     });
     app.get('/pro*ar', function (req, res) {
         res.send('Respuesta al patrón pro*ar');
+    });
+    app.get("/songs", function(req, res){
+        let songs=[{
+            "title": "Blank space",
+            "price": "1.2"
+        }, {
+            "title": "See you again",
+            "price": "1.3"
+        }, {
+            "title": "Uptown Funk",
+            "price": "1.1"
+        }];
+
+        let response = {
+            seller: 'Tienda de canciones',
+            songs:songs
+        };
+        res.render("shop.twig", response)
     });
 
 
