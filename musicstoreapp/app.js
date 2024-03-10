86218d81b8cb9b71a87998e9c8c2e48df084d5c0
lauratbg <UO275725@uniovi.es>
2024-03-10 19:57:08 +0100

    SDI-2324-207-8.4-Recursos estáticos, peticiones POST y parámetros, Enrutamiento y comodines.

--- musicstoreapp/app.js
+++ musicstoreapp/app.js
@@ -9,7 +9,13 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+let bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 require("./routes/songs.js")(app);
+
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
